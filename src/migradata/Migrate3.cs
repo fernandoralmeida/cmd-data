9c68cc032de166cd2ae32152cdffe2773678c625
fernandoalmeida <fernando.ralmeida@outlook.com>
2023-03-29 22:29:02 -0300

    up-migra3

--- src/migradata/Migrate3.cs
+++ src/migradata/Migrate3.cs
@@ -778,7 +778,7 @@ public class Migrate3
                     });
 
                     await Task.WhenAll(T1, T2, T3, T4, T5, T6, T7, T8);
-                    i++;
+                    
                 }
                 catch (Exception ex)
                 {
